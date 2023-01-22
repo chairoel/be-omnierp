9204211a3bb8d696979b60fd08ffd1ce5d15bcde
Isa Subani <isa.subani@nostratech.com>
2023-01-22 12:38:53 +0700

    add unit testing

--- omnierp-app/src/test/java/com/fluex404/omnierpapp/OmnierpAppApplicationTests.java
+++ omnierp-app/src/test/java/com/fluex404/omnierpapp/OmnierpAppApplicationTests.java
@@ -1,13 +1,14 @@
-//package com.fluex404.omnierpapp;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class OmnierpAppApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package com.fluex404.omnierpapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmnierpAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("TEST UNIT TESTING");
+    }
+
+}
