0362957643795ea8d5fb09deda9dc6e87675e1d9
Isa <isaabqari@gmail.com>
2023-02-18 00:58:48 +0700

    benerin run

--- run.bash
+++ run.bash
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 docker build -t be-omnierp . &&
-docker run --name be-omnierp --restart always -p 8585:8080 be-omnierp
+docker run -it -d --name be-omnierp --restart always -p 8585:8080 be-omnierp
