52e007f3d5d1f9e95fecf889e36d8b811ed2271a
Isa <isaabqari@gmail.com>
2023-02-18 13:10:23 +0700

    entahlah

--- run.bash
+++ run.bash
@@ -1,4 +1,4 @@
 #! /bin/bash
 mvn install clean package &&
-docker build -t --no-cache be-omnierp . &&
+docker build --no-cache -t be-omnierp . &&
 docker run -it -d --name be-omnierp --restart always -p 8585:8080 be-omnierp
